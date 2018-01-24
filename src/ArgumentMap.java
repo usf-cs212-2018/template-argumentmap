755f0e8810f6c3ac85ada88425de8ba5126d2dc0
chris bryan <this.is.cjb@gmail.com>
2018-01-24 15:52:10 -0800

    first commit

---
+++ src/ArgumentMap.java
@@ -0,0 +1,146 @@
+import java.util.HashMap;
+import java.util.Map;
+
+// TODO Homework: Fill in or fix all of the methods with TODO comments.
+
+/**
+ * Parses command-line arguments into flag/value pairs, and stores those pairs
+ * in a map for easy access.
+ */
+public class ArgumentMap {
+
+	private final Map<String, String> map;
+
+	/**
+	 * Initializes the argument map.
+	 */
+	public ArgumentMap() {
+		map = new HashMap<>();
+	}
+
+	/**
+	 * Initializes the argument map and parses the specified arguments into
+	 * key/value pairs.
+	 *
+	 * @param args
+	 *            command line arguments
+	 *
+	 * @see #parse(String[])
+	 */
+	public ArgumentMap(String[] args) {
+		this();
+		parse(args);
+	}
+
+	/**
+	 * Parses the specified arguments into key/value pairs and adds them to the
+	 * argument map.
+	 *
+	 * @param args
+	 *            command line arguments
+	 */
+	public void parse(String[] args) {
+		// TODO
+	}
+
+	/**
+	 *
+	 * @param arg
+	 * @return
+	 */
+	public static boolean isFlag(String arg) {
+		return false; // TODO
+	}
+
+	/**
+	 *
+	 * @param arg
+	 * @return
+	 */
+	public static boolean isValue(String arg) {
+		return false; // TODO
+	}
+
+	/**
+	 * Returns the number of unique flags stored in the argument map.
+	 *
+	 * @return number of flags
+	 */
+	public int numFlags() {
+		return -1; // TODO (1 LOC)
+	}
+
+	/**
+	 * Determines whether the specified flag is stored in the argument map.
+	 *
+	 * @param flag
+	 *            flag to test
+	 *
+	 * @return true if the flag is in the argument map
+	 */
+	public boolean hasFlag(String flag) {
+		return false; // TODO (1 LOC)
+	}
+
+	/**
+	 * Determines whether the specified flag is stored in the argument map and
+	 * has a non-null value stored with it.
+	 *
+	 * @param flag
+	 *            flag to test
+	 *
+	 * @return true if the flag is in the argument map and has a non-null value
+	 */
+	public boolean hasValue(String flag) {
+		return false; // TODO (1 LOC)
+	}
+
+	/**
+	 * Returns the value for the specified flag as a String object.
+	 *
+	 * @param flag
+	 *            flag to get value for
+	 *
+	 * @return value as a String or null if flag or value was not found
+	 */
+	public String getString(String flag) {
+		return null; // TODO (1 LOC)
+	}
+
+	/**
+	 * Returns the value for the specified flag as a String object. If the flag
+	 * is missing or the flag does not have a value, returns the specified
+	 * default value instead.
+	 *
+	 * @param flag
+	 *            flag to get value for
+	 * @param defaultValue
+	 *            value to return if flag or value is missing
+	 * @return value of flag as a String, or the default value if the flag or
+	 *         value is missing
+	 */
+	public String getString(String flag, String defaultValue) {
+		return null; // TODO
+	}
+
+	/**
+	 * Returns the value for the specified flag as an int value. If the flag is
+	 * missing or the flag does not have a value, returns the specified default
+	 * value instead.
+	 *
+	 * @param flag
+	 *            flag to get value for
+	 * @param defaultValue
+	 *            value to return if the flag or value is missing
+	 * @return value of flag as an int, or the default value if the flag or
+	 *         value is missing
+	 */
+	public int getInteger(String flag, int defaultValue) {
+		return -1; // TODO
+	}
+
+	@Override
+	public String toString() {
+		return map.toString();
+	}
+}
---
+++ test/ArgumentMapTest.java
@@ -0,0 +1,452 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
+@RunWith(Enclosed.class)
+public class ArgumentMapTest {
+
+	@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+	public static class FlagTest {
+
+		@Test
+		public void testFlag01() {
+			String test = "-a";
+			boolean actual = ArgumentMap.isFlag(test);
+			Assert.assertTrue(actual);
+		}
+
+		@Test
+		public void testFlag02() {
+			String test = "-1";
+			boolean actual = ArgumentMap.isFlag(test);
+			Assert.assertTrue(actual);
+		}
+
+		@Test
+		public void testFlag03() {
+			String test = "-hello";
+			boolean actual = ArgumentMap.isFlag(test);
+			Assert.assertTrue(actual);
+		}
+
+		@Test
+		public void testFlag04() {
+			String test = "--world";
+			boolean actual = ArgumentMap.isFlag(test);
+			Assert.assertTrue(actual);
+		}
+
+		@Test
+		public void testValue01() {
+			String test = "a";
+			boolean actual = ArgumentMap.isFlag(test);
+			Assert.assertFalse(actual);
+		}
+
+		@Test
+		public void testValue02() {
+			String test = "1";
+			boolean actual = ArgumentMap.isFlag(test);
+			Assert.assertFalse(actual);
+		}
+
+		@Test
+		public void testValue03() {
+			String test = "a-b-c";
+			boolean actual = ArgumentMap.isFlag(test);
+			Assert.assertFalse(actual);
+		}
+
+		@Test
+		public void testValue04() {
+			String test = "hello";
+			boolean actual = ArgumentMap.isFlag(test);
+			Assert.assertFalse(actual);
+		}
+
+		@Test
+		public void testValue05() {
+			String test = "hello world";
+			boolean actual = ArgumentMap.isFlag(test);
+			Assert.assertFalse(actual);
+		}
+
+		@Test
+		public void testInvalid01() {
+			String test = "";
+			boolean actual = ArgumentMap.isFlag(test);
+			Assert.assertFalse(actual);
+		}
+
+		@Test
+		public void testInvalid02() {
+			String test = " ";
+			boolean actual = ArgumentMap.isFlag(test);
+			Assert.assertFalse(actual);
+		}
+
+		@Test
+		public void testInvalid03() {
+			String test = "\t";
+			boolean actual = ArgumentMap.isFlag(test);
+			Assert.assertFalse(actual);
+		}
+
+		@Test
+		public void testInvalid04() {
+			String test = "-";
+			boolean actual = ArgumentMap.isFlag(test);
+			Assert.assertFalse(actual);
+		}
+
+		@Test
+		public void testInvalid05() {
+			String test = "- ";
+			boolean actual = ArgumentMap.isFlag(test);
+			Assert.assertFalse(actual);
+		}
+
+		@Test
+		public void testInvalid06() {
+			String test = null;
+			boolean actual = ArgumentMap.isFlag(test);
+			Assert.assertFalse(actual);
+		}
+	}
+
+	@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+	public static class ValueTest {
+
+		@Test
+		public void testFlag01() {
+			String test = "-a";
+			boolean actual = ArgumentMap.isValue(test);
+			Assert.assertFalse(actual);
+		}
+
+		@Test
+		public void testFlag02() {
+			String test = "-1";
+			boolean actual = ArgumentMap.isValue(test);
+			Assert.assertFalse(actual);
+		}
+
+		@Test
+		public void testFlag03() {
+			String test = "-hello";
+			boolean actual = ArgumentMap.isValue(test);
+			Assert.assertFalse(actual);
+		}
+
+		@Test
+		public void testFlag04() {
+			String test = "--world";
+			boolean actual = ArgumentMap.isValue(test);
+			Assert.assertFalse(actual);
+		}
+
+		@Test
+		public void testValue01() {
+			String test = "a";
+			boolean actual = ArgumentMap.isValue(test);
+			Assert.assertTrue(actual);
+		}
+
+		@Test
+		public void testValue02() {
+			String test = "1";
+			boolean actual = ArgumentMap.isValue(test);
+			Assert.assertTrue(actual);
+		}
+
+		@Test
+		public void testValue03() {
+			String test = "a-b-c";
+			boolean actual = ArgumentMap.isValue(test);
+			Assert.assertTrue(actual);
+		}
+
+		@Test
+		public void testValue04() {
+			String test = "hello";
+			boolean actual = ArgumentMap.isValue(test);
+			Assert.assertTrue(actual);
+		}
+
+		@Test
+		public void testValue05() {
+			String test = "hello world";
+			boolean actual = ArgumentMap.isValue(test);
+			Assert.assertTrue(actual);
+		}
+
+		@Test
+		public void testInvalid01() {
+			String test = "";
+			boolean actual = ArgumentMap.isValue(test);
+			Assert.assertFalse(actual);
+		}
+
+		@Test
+		public void testInvalid02() {
+			String test = " ";
+			boolean actual = ArgumentMap.isValue(test);
+			Assert.assertFalse(actual);
+		}
+
+		@Test
+		public void testInvalid03() {
+			String test = "\t";
+			boolean actual = ArgumentMap.isValue(test);
+			Assert.assertFalse(actual);
+		}
+
+		@Test
+		public void testInvalid04() {
+			String test = "-";
+			boolean actual = ArgumentMap.isValue(test);
+			Assert.assertFalse(actual);
+		}
+
+		@Test
+		public void testInvalid05() {
+			String test = "- ";
+			boolean actual = ArgumentMap.isValue(test);
+			Assert.assertFalse(actual);
+		}
+
+		@Test
+		public void testInvalid06() {
+			String test = null;
+			boolean actual = ArgumentMap.isValue(test);
+			Assert.assertFalse(actual);
+		}
+	}
+
+	@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+	public static class CountTest {
+
+		@Test
+		public void test01() {
+			String[] args = { "-loquat" };
+			int expected = 1;
+			int actual = new ArgumentMap(args).numFlags();
+			Assert.assertEquals(expected, actual);
+		}
+
+		@Test
+		public void test02() {
+			String[] args = { "-grape", "raisin" };
+			int expected = 1;
+			int actual = new ArgumentMap(args).numFlags();
+			Assert.assertEquals(expected, actual);
+		}
+
+		@Test
+		public void test03() {
+			String[] args = { "-tomato", "-potato" };
+			int expected = 2;
+			int actual = new ArgumentMap(args).numFlags();
+			Assert.assertEquals(expected, actual);
+		}
+
+		@Test
+		public void test04() {
+			String[] args = { "rhubarb" };
+			int expected = 0;
+			int actual = new ArgumentMap(args).numFlags();
+			Assert.assertEquals(expected, actual);
+		}
+
+		@Test
+		public void test05() {
+			String[] args = { "constant", "change" };
+			int expected = 0;
+			int actual = new ArgumentMap(args).numFlags();
+			Assert.assertEquals(expected, actual);
+		}
+
+		@Test
+		public void test06() {
+			String[] args = { "pine", "-apple" };
+			int expected = 1;
+			int actual = new ArgumentMap(args).numFlags();
+			Assert.assertEquals(expected, actual);
+		}
+
+		@Test
+		public void test07() {
+			String[] args = { "-aubergine", "eggplant", "-courgette", "zucchini" };
+			int expected = 2;
+			int actual = new ArgumentMap(args).numFlags();
+			Assert.assertEquals(expected, actual);
+		}
+
+		@Test
+		public void test08() {
+			String[] args = { "-tangerine", "satsuma", "-tangerine", "clementine", "-tangerine", "mandarin" };
+			int expected = 1;
+			int actual = new ArgumentMap(args).numFlags();
+			Assert.assertEquals(expected, actual);
+		}
+
+		@Test
+		public void test09() {
+			String[] args = {};
+			int expected = 0;
+			int actual = new ArgumentMap(args).numFlags();
+			Assert.assertEquals(expected, actual);
+		}
+
+		// it is okay to throw a null pointer exception here
+		@Test(expected = java.lang.NullPointerException.class)
+		public void test10() {
+			String[] args = null;
+			int expected = 0;
+			int actual = new ArgumentMap(args).numFlags();
+			Assert.assertEquals(expected, actual);
+		}
+	}
+
+	@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+	public static class ParseTest {
+		private ArgumentMap map;
+		private String debug;
+
+		@Before
+		public void setup() {
+			String[] args = { "-a", "42", "-b", "bat", "cat", "-d", "-e", "elk", "-e", "-f" };
+
+			map = new ArgumentMap();
+			map.parse(args);
+
+			debug = "\n" + map.toString() + "\n";
+		}
+
+		@After
+		public void teardown() {
+			map = null;
+		}
+
+		@Test
+		public void testNumFlags() {
+			int expected = 5;
+			int actual = map.numFlags();
+
+			Assert.assertEquals(debug, expected, actual);
+		}
+
+		@Test
+		public void testHasFlag() {
+			Assert.assertTrue(debug, map.hasFlag("-d"));
+		}
+
+		@Test
+		public void testHasLastFlag() {
+			Assert.assertTrue(debug, map.hasFlag("-f"));
+		}
+
+		@Test
+		public void testHasntFlag() {
+			Assert.assertFalse(debug, map.hasFlag("-g"));
+		}
+
+		@Test
+		public void testHasValue() {
+			Assert.assertTrue(debug, map.hasValue("-a"));
+		}
+
+		@Test
+		public void testHasFlagNoValue() {
+			Assert.assertFalse(debug, map.hasValue("-d"));
+		}
+
+		@Test
+		public void testNoFlagNoValue() {
+			Assert.assertFalse(debug, map.hasValue("-g"));
+		}
+
+		@Test
+		public void testGetValueExists() {
+			String expected = "bat";
+			String actual = map.getString("-b");
+			Assert.assertEquals(debug, expected, actual);
+		}
+
+		@Test
+		public void testGetValueNull() {
+			String expected = null;
+			String actual = map.getString("-d");
+			Assert.assertEquals(debug, expected, actual);
+		}
+
+		@Test
+		public void testGetValueNoFlag() {
+			String expected = null;
+			String actual = map.getString("-g");
+			Assert.assertEquals(debug, expected, actual);
+		}
+
+		@Test
+		public void testGetValueRepeatedFlag() {
+			String expected = null;
+			String actual = map.getString("-e");
+			Assert.assertEquals(debug, expected, actual);
+		}
+
+		@Test
+		public void testGetDefaultExists() {
+			String expected = "bat";
+			String actual = map.getString("-b", "bee");
+			Assert.assertEquals(debug, expected, actual);
+		}
+
+		@Test
+		public void testGetDefaultNull() {
+			String expected = "dog";
+			String actual = map.getString("-d", "dog");
+			Assert.assertEquals(debug, expected, actual);
+		}
+
+		@Test
+		public void testGetDefaultMissing() {
+			String expected = "goat";
+			String actual = map.getString("-g", "goat");
+			Assert.assertEquals(debug, expected, actual);
+		}
+
+		@Test
+		public void testGetIntOkay() {
+			int expected = 42;
+			int actual = map.getInteger("-a", 42);
+			Assert.assertEquals(debug, expected, actual);
+		}
+
+		@Test
+		public void testGetIntNotOkay() {
+			int expected = 42;
+			int actual = map.getInteger("-b", 42);
+			Assert.assertEquals(debug, expected, actual);
+		}
+
+		@Test
+		public void testGetIntNull() {
+			int expected = 42;
+			int actual = map.getInteger("-f", 42);
+			Assert.assertEquals(debug, expected, actual);
+		}
+
+		@Test
+		public void testGetIntMissing() {
+			int expected = 42;
+			int actual = map.getInteger("-g", 42);
+			Assert.assertEquals(debug, expected, actual);
+		}
+	}
+
+}
